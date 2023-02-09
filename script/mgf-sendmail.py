0a8b1b3f91ae32825cca5db3c71826e3e8e14512
mgfalkus <mgf@selb.st>
2023-02-09 21:23:25 +0100

    Finalized email script

--- script/mgf-sendmail.py
+++ script/mgf-sendmail.py
@@ -11,73 +11,95 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from getpass import getpass
 from icalendar import Calendar, Event
+from os.path import abspath
 from smtplib import SMTP
 from uuid import uuid4
 from yaml import safe_load
 from zoneinfo import ZoneInfo
 
-header = '''
-Dear Berlin Security Community,
-
-TL;DR | Join the next Security Night on September 2nd from 7 pm CEST at c-base:  Rungestraße 20 10179 Berlin. Three speakers are ready to share their talks with you:
-
-1. Something Something hashes by pspacecomplete
-2. Soemthing Somethin Windows Server hacking by Sven
-3. Somethin Somethin SSHFP ny Neef
-
---> WHAT TO EXPECT
-'''.lstrip()
-
-footer = '''
---> THE TALKS
-
-(1) "TBD"
-
-(2) "TBD"
-
-(3) "TBD"
-
---> THE NIGHT
-
-We are happy to invite you to our next Security Night on September 8 at SysEleven's offices: Boxhagener Str. 79, 10245 Berlin (https://openstreetmap.org/node/2825036679). Our agenda for the night:
-
-7:00 pm -- Welcome
-7:10 pm -- TBD
-7:30 pm -- TBD
-7:50 pm -- TBD
-8:10 pm -- Q&A
-9:00 pm -- Closing
-
-Attendance is free. Emergency phone number, in case of any problems: +4917634326568. Please register at Meetup, so we can plan accordingly: https://meetup.com/berlin-internet-security-group/events/287899582
-
-Please note the regulations regarding COVID-19 on the day of the event. If you want, you can of course wear a mask and/or register on site via the Corona-Warn-App.
-
-See you there!
-
---> CONTACT US
-
-Berlin Security Nights are organized by Martin, Akendo and Hendrik as a contribution to the scene. We like bringing great people together. You can find us offline at our nights or online on Slack:
-https://join.slack.com/t/berlin-infosec/shared_invite/enQtNTY3ODU0OTU5NjcwLTAzMmZiNDQxNDk0NzE4NGJjOTE0ODJiOWRkMGY2Y2QwZTUxYzgzYTVlMGQ3YTllNjQ0YjFiNzVlYjZiMWU2MWY
-
-Your (lexicographically ordered) Security Night Owls
-Akendo, Hendrik, Martin
-
-PS: We are always looking for interesting talks and projects. If you have a talk proposal, an interesting project or something you would love to share with the community, please write us an email or reach out on Slack.
-'''.lstrip()
+def gen_text(talks, space, time_start, duration, tldr, invitation, meetup_url, contacts):
+    text = 'Dear Berlin Security Community,\n\n'
+    text += 'TL;DR | ' + tldr + '\n\n'
+    text += '--> WHAT TO EXPECT\n\n'
+    text += invitation + '\n\n'
+    text += '--> THE TALKS\n\n'
+    for talk_id in list(talks):
+        text += '(' + str(talk_id) + ') "' + talks[talk_id]['title'] + '"'
+        text += ' by ' + talks[talk_id]['name'] + ':\n'
+        text += talks[talk_id]['abstract'] + '\n\n'
+    text += '--> THE NIGHT\n\n'
+    text += 'We are happy to invite you to our next Security Night conference'
+    text += ' on ' + time_start.strftime('%B %-d')
+    text += ' at ' + space['name'] + ': ' + space['address'] + ' (' + space['map_url'] + ').'
+    text += ' Our agenda for this Night:\n\n'
+    time_current = time_start
+    text += time_current.strftime('%I:%M %p') + ' -- Welcome\n'
+    time_current += timedelta(minutes = 10)
+    for talk_id in list(talks):
+        text += time_current.strftime('%I:%M %p') + ' -- ' + talks[talk_id]['name'] + '\'s talk\n'
+        time_current += timedelta(minutes = 20)
+        if talk_id < len(list(talks)):
+            time_current += timedelta(minutes = 10)
+    text += time_current.strftime('%I:%M %p') + ' -- Networking\n'
+    time_current = time_start + timedelta(minutes = duration)
+    text += time_current.strftime('%I:%M %p') + ' -- Closing\n\n'
+    text += 'Attendance is free. Emergency phone number, in case of any problems: ' + ', '.join(contacts) + '.'
+    text += ' Please register at Meetup, so we can plan accordingly: ' + meetup_url + '\n\n'
+    text += '--> CONTACT US\n\n'
+    text += 'Berlin Security Nights are organized by Akendo, Hendrik and Martin as a contribution to the scene.'
+    text += ' We like bringing great people together. You can find us offline at our Nights or online on Slack:'
+    text += ' https://join.slack.com/t/berlin-infosec/shared_invite/enQtNTY3ODU0OTU5NjcwLTAzMmZiNDQxNDk0NzE4NGJjOTE0ODJiOWRkMGY2Y2QwZTUxYzgzYTVlMGQ3YTllNjQ0YjFiNzVlYjZiMWU2MWY\n\n'
+    text += 'See you around!\n\n'
+    text += 'Your Security Night Owls,\n'
+    text += 'Akendo, Hendrik, Martin\n\n'
+    text += 'PS: We are always looking for interesting talks and projects.'
+    text += ' If you have a talk proposal, an interesting project or something you would love to share with the community, please write us an email or reach out on Slack.\n\n'
+    text += '-- \nTo unsubscribe from the mailing list, please reply to this email with "unsubscribe" in the subject or just as the message body.\n'
+    return text
 
 def get_input_data():
-    event_data = {}
-    print('Please make sure the event\'s YAML data is in place!')
-    event_data['id'] = input('Event ID, e.g. "2023/March": ')
-    with open('../' + event_data['id'] + '/event.yaml') as f:
-        event_data.update(safe_load(f))
-    event_data['time'] = datetime.strptime(event_data['time'], '%Y-%m-%dT%H:%M:%S%z')
-    with open('../' + event_data['id'] + '/talks.yaml') as f:
-        event_data['talks'] = safe_load(f)
-    with open('../' + event_data['id'] + '/invitation.txt') as f:
-        event_data['invitation'] = header + '\n'
-        event_data['invitation'] += f.read() + '\n'
-        event_data['invitation'] += footer + '\n'
+    event = {}
+    event['id'] = input('Please input the event ID, e.g. "2023/March": ')
+    file_event = '../' + event['id'] + '/event.yaml'
+    file_talks = '../' + event['id'] + '/talks.yaml'
+    file_invitation = '../' + event['id'] + '/invitation.txt'
+    file_output = '../' + event['id'] + '/email_body.txt'
+    print('Haye you prepared the following files?')
+    print('--> ' + abspath(file_event))
+    print('--> ' + abspath(file_talks))
+    print('--> ' + abspath(file_invitation))
+    if input('If so, type "yes": ').lower() != 'yes':
+        raise Exception('Event data not yet prepared')
+    with open(file_event) as f:
+        event.update(safe_load(f))
+    event['time'] = datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%S%z')
+    with open(file_talks) as f:
+        event['talks'] = safe_load(f)
+    event['tldr'] = 'Join our next Security Night conference'
+    event['tldr'] += ' on ' + event['time'].strftime('%B %-d from %-I %p %Z')
+    event['tldr'] += ' at ' + event['space']['name'] + ': ' + event['space']['address'] + '.'
+    event['tldr'] += ' The following speakers are ready to share their insights with you:\n'
+    for talk_id in list(event['talks']):
+        event['tldr'] += '\n' + str(talk_id) + '. "' + event['talks'][talk_id]['title'] + '"'
+        event['tldr'] += ' by ' + event['talks'][talk_id]['name']
+    with open(file_invitation) as f:
+        event['text'] = gen_text(
+            event['talks'],
+            event['space'],
+            event['time'],
+            event['duration'],
+            event['tldr'],
+            f.read(),
+            event['meetup_url'],
+            event['contacts']
+        )
+    with open(file_output, 'w') as f:
+        f.write(event['text'])
+    print('Does the following file represent your intended email?')
+    print('--> ' + abspath(file_output))
+    if input('If so, type "yes": ').lower() != 'yes':
+        raise Exception('Generated email body inappropriate')
+    print('Nice! Please input some more data:')
     return {
         'server': {
             'address': input('Server address: '),
@@ -89,7 +111,7 @@ def get_input_data():
             'name': input('Sender name: '),
             'address': input('Email address: ')
         },
-        'event': event_data
+        'event': event
     }
 
 def gen_cal_event(id, topic, space, time, duration, url, text):
@@ -97,26 +119,38 @@ def gen_cal_event(id, topic, space, time, duration, url, text):
     calendar.add('PRODID', '-//mgf//NONSGML sendmail v0.1//EN')
     calendar.add('VERSION', '2.0')
     event = Event()
-    event.add('SUMMARY', 'Security Night ' + id + ': ' + topic)
     event.add('UID', uuid4())
     event.add('DTSTAMP', datetime.utcnow())
+    event.add('SUMMARY', 'Security Night ' + id + ': ' + topic)
+    event.add('LOCATION', space['name'] + ', ' + space['address'])
     event.add('DTSTART', time)
     event.add('DTEND', time + timedelta(minutes = duration))
-    event.add('LOCATION', space['name'] + ', ' + space['address'])
-    event.add('DESCRIPTION', text)
     event.add('URL', url)
+    event.add('DESCRIPTION', text)
     calendar.add_component(event)
     return calendar.to_ical()
 
-def gen_message(sender, event, image, ical):
-    subject = '[Security Nights] Invitation ' + event['id'] + ': ' + event['topic']
-    subject += ' -- ' + event['time'].strftime('%A, %B %-d, %Y, %-I %p %Z') + ' @ ' + event['space']['name']
+def gen_message(sender, event, image):
     message = MIMEMultipart()
     message['Date'] = datetime.utcnow().strftime('%a, %-d %b %Y %X +0000')
     message['From'] = sender['name'] + ' <' + sender['address'] + '>'
+    subject = '[Security Nights] Invitation ' + event['id'] + ': ' + event['topic']
+    subject += ' -- ' + event['time'].strftime('%A, %B %-d, %Y, %-I %p %Z') + ' @ ' + event['space']['name']
     message['Subject'] = subject
-    message.attach(MIMEText(event['invitation'].replace('\n', '\r\n'), _charset = 'utf-8'))
-    part_calendar = MIMEText(ical.decode(), _subtype = 'calendar', _charset = 'utf-8')
+    message.attach(MIMEText(event['text'].replace('\n', '\r\n'), _charset = 'utf-8'))
+    part_calendar = MIMEText(
+        gen_cal_event(
+            event['id'],
+            event['topic'],
+            event['space'],
+            event['time'],
+            event['duration'],
+            event['meetup_url'],
+            event['tldr']
+        ).decode(),
+        _subtype = 'calendar',
+        _charset = 'utf-8'
+    )
     part_calendar['Content-Disposition'] = 'attachment; filename="SN_' + event['id'].replace('/', '-') + '.ics"'
     message.attach(part_calendar)
     part_image = MIMEImage(image, _subtype = 'jpeg')
@@ -126,29 +160,9 @@ def gen_message(sender, event, image, ical):
 
 def main():
     data = get_input_data()
-    tldr = 'Join our next Security Night'
-    tldr += ' on ' + data['event']['time'].strftime('%B %-d from %-I %p %Z')
-    tldr += ' at ' + data['event']['space']['name'] + ': ' + data['event']['space']['address'] + '.'
-    tldr += ' The following speakers are ready to share their talks with you:\n'
-    for talk_id in list(data['event']['talks']):
-        tldr += '\n' + str(talk_id) + '. "' + data['event']['talks'][talk_id]['title'] + '"'
-        tldr += ' by ' + data['event']['talks'][talk_id]['name']
-    with open('banner.jpeg', 'rb') as f:
-        message = gen_message(
-            data['sender'],
-            data['event'],
-            f.read(),
-            gen_cal_event(
-                data['event']['id'],
-                data['event']['topic'],
-                data['event']['space'],
-                data['event']['time'],
-                data['event']['duration'],
-                data['event']['meetup_url'],
-                tldr
-            )
-        )
     recipients = []
+    with open('banner.jpeg', 'rb') as f:
+        message = gen_message(data['sender'], data['event'], f.read())
     with open('recipients.txt') as f:
         for line in f.readlines():
             recipients.append(line.rstrip())
